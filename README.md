5e1ad3efa9398468fcb1a449c59c73de7621ab50
zhangweixiao <zhangweixiao@live.com>
2019-03-06 16:14:22 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-1. 使用html-webpack-plugin自动生成html
-2. webpack loader基础使用
+### 服务端渲染
+
+1
---
+++ build/webpack.config.server.js
@@ -0,0 +1,28 @@
+const path=require('path');
+const HTMLPlugin = require('html-webpack-plugin');
+
+module.exports={
+    target: 'node',//在nodejs环境中执行
+    entry: {
+        app: path.join(__dirname,"../client/server-entry.js")
+    },
+    output:{
+        filename: 'server-entry.js',
+        path: path.join(__dirname,'../dist'),
+        libraryTarget: "commonjs2"
+    },
+    module: {
+        rules: [{
+            test: /.jsx$/,
+            loader: 'babel-loader' //js编译器
+        }, {
+            test: /.js$/,
+            loader: 'babel-loader', //js编译器
+            exclude:[
+                path.join(__dirname,'../node_modules')
+            ]
+        }
+        ]
+    },
+
+};
